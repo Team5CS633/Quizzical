7bd55834ff6bffb8031dc6af06e8b2ed5990263e
Team5CS633 <60205481+Team5CS633@users.noreply.github.com>
2020-01-30 20:19:04 -0500

    Add files via upload

--- quiz.php
+++ quiz.php
@@ -1,16 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Quizzical</title>
+    <!-- Bootstrap CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
 <?php
 include_once 'config.php';
 
 if (@$_GET['page'] == 'quiz') {
 
     $eid   = @$_GET['eid'];
-    $q     = mysqli_query($link, "SELECT * FROM questions WHERE eid='$eid' ");
+
+    $s          = mysqli_query($link, "SELECT * FROM quiz WHERE eid='$eid' ");
+    $sQuiz      = mysqli_fetch_array($s);
+    $quizName   = $sQuiz['title'];
 
     echo '
-        <div class="panel" style="margin-right:5%;margin-left:5%;margin-top:10px;border-radius:10px">
-        <form id="qform" action="update.php?page=quiz&step=2&eid=' . $eid . '&n=' . $sn . '&t=' . $total . '&qid=' . $qid . '" method="POST"  class="form-horizontal">
-        <br />
+        <div class="container">
+            <div class="text-center">
+                <h1>' . $quizName . '</h1>
+            </div>
+            <br>
     ';
+
+    echo '
+            <form id="qform" action="update.php?page=quiz&step=2&eid=' . $eid . '&n=' . $sn . '&t=' . $total . '&qid=' . $qid . '" method="POST"  class="form-horizontal">
+    ';
+
+    $q     = mysqli_query($link, "SELECT * FROM questions WHERE eid='$eid' ");
 
     while ($row = mysqli_fetch_array($q)) {
         $qns = stripslashes($row['qns']);
@@ -18,11 +45,14 @@ if (@$_GET['page'] == 'quiz') {
         $sn = $row['sn'];
 
         echo '
-            <b><pre style="background-color:white"><div style="font-size:20px;font-weight:bold;font-family:calibri;margin:10px">' . $sn . ' : ' . $qns . '</div></pre></b>
+            <div class="card">
+                <div class="card-header">Question ' . $sn . ' - ' . $qns . '
+                </div>
+                <div class="card-body">
         ';
 
         echo '
-            <div class="funkyradio">
+                    <div class="funkyradio">
         ';
 
         $k = mysqli_query($link, "SELECT * FROM options WHERE qid='$qid' ");
@@ -30,35 +60,41 @@ if (@$_GET['page'] == 'quiz') {
         while ($row = mysqli_fetch_array($k)) {
             $option   = stripslashes($row['option']);
             $optionid = $row['optionid'];
+
             echo '
-                <div class="funkyradio-success">
-                    <input type="radio" id="' . $optionid . '" name="ans" value="' . $optionid . '" onclick="enable()">
-                    <label for="' . $optionid . '" style="width:50%">
-                    <div style="color:black;font-size:12px;word-wrap:break-word">&nbsp;&nbsp;' . $option . '
-                    </div>
-                    </label>
-                </div>
+                        <div class="funkyradio-success">
+                            <input type="radio" id="' . $optionid . '" name="ans" value="' . $optionid . '" onclick="enable()">
+                            <label for="' . $optionid . '" style="width:50%">' . $option . '</label>
+                        </div>
             ';
         }
 
         echo '
+                    </div>
+        ';
+
+        echo '
+                </div>
             </div>
+            <br>
         ';
         
     }
  
     echo '
-        <br>
-        <button type="submit" class="btn btn-default" disabled="true" id="sbutton" style="height:30px">
-        <span class="glyphicon glyphicon-lock" style="font-size:12px" aria-hidden="true"></span>
-        <font style="font-size:12px;font-weight:bold">Submit<font>
-        </button>
-        <a href="account.php?q=quiz&step=2&eid=' . $eid . '&n=' . ($sn + 1) . '&t=' . $total . '" class="btn btn-primary" style="height:30px">
-        <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"  style="font-size:12px"></span>
-        </a></form>
+            <div class="text-center">
+            <button type="submit" class="btn btn-default" disabled="true" id="sbutton" style="height:30px">
+                <font style="font-size:12px;font-weight:bold">Submit<font>
+            </button>
+            </form>
+            </div>
         </div>
+
     ';
 
 }
 
 ?>
+
+</body>
+</html>
